6c5e14d47cd7f8619b62a7345583f5f949387044
smayanja3 <mayanjaswabira247@gmail.com>
2026-09-18 19:26:06 -0400

    Edited CSS HTML

---
+++ js/tictactoe.js
@@ -0,0 +1,11 @@
+// create two players
+// create 9 inputs and each has its own click event
+// click to place there X or O
+//restart??? or just refresh
+//
+// if they win = "You Won" = IN THE DOM 
+
+
+//Rules
+    // players = Characters I think 
+    //
